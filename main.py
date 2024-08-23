a820f8ecda7b2ea5261d7c4147ec3887f161f53e
Le-Chat99 <bgkhiem291@gmail.com>
2024-08-23 14:23:06 +0700

    game window

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while {
-        
-    }
-
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    
 
 
 if __name__ == "__main__":
